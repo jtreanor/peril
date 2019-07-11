6ad5718e947b16f19ef2cc5ad497f400fa3ba3f1
James Treanor <jtreanor3@gmail.com>
2019-07-11 14:49:55 +0100

    Cache Github tokens

--- api/source/api/github.ts
+++ api/source/api/github.ts
@@ -3,9 +3,10 @@ import { PERIL_INTEGRATION_ID, PRIVATE_GITHUB_SIGNING_KEY } from "../globals"
 import { App } from "@octokit/app"
 import winston from "../logger"
 
+export const GithubApp = new App({ id: PERIL_INTEGRATION_ID, privateKey: PRIVATE_GITHUB_SIGNING_KEY })
+
 export async function getTemporaryAccessTokenForInstallation(installationId: number): Promise<string> {
-  const app = new App({ id: parseInt(PERIL_INTEGRATION_ID, 10), privateKey: PRIVATE_GITHUB_SIGNING_KEY })
-  const token = await app.getInstallationAccessToken({ installationId })
+  const token = await GithubApp.getInstallationAccessToken({ installationId })
   winston.info(`[github auth] - ${token}`)
   return token
 }
